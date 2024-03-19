86a73bbf068e019ce69009a4f8983369706c1852
Jose Arturo Sierra Govea <artugamer8@gmail.com>
2024-03-19 11:33:18 -0600

    19 de Marzo

--- dist/controllers/usuarioController.js
+++ dist/controllers/usuarioController.js
@@ -13,6 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.usuarioController = void 0;
+const validator_1 = __importDefault(require("validator"));
 const usuarioModelo_1 = __importDefault(require("../models/usuarioModelo"));
 const utils_1 = require("../utils/utils");
 class UsuarioController {
@@ -29,22 +30,35 @@ class UsuarioController {
     add(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                const usuario = req.body; // Suponiendo que el usuario se envía en el cuerpo de la solicitud
-                const existeUsuario = yield usuarioModelo_1.default.getByEmail(usuario.email); // Llama al método estático getByEmail
+                const usuario = req.body;
+                if (!usuario.email) {
+                    return res.status(400).json({ message: "Falta correo electrónico", code: 1 });
+                }
+                if (!usuario.password) {
+                    return res.status(400).json({ message: "Falta contraseña", code: 1 });
+                }
+                if (!validator_1.default.isEmail(usuario.email)) {
+                    return res.status(400).json({ message: "El correo electrónico no es válido", code: 1 });
+                }
+                if (!usuario.role || !usuario.role) {
+                    return res.status(400).json({ message: "Ingresa un rol", code: 1 });
+                }
+                if (!validator_1.default.isLength(usuario.password, { min: 6 })) {
+                    return res.status(400).json({ message: "Contraseña invalida, debe tener al menos 6 caracteres", code: 1 });
+                }
+                const existeUsuario = yield usuarioModelo_1.default.getByEmail(usuario.email);
                 if (existeUsuario) {
-                    return res.status(400).json({ message: "Ya existe un usuario con el mismo correo electrónico", code: 1 });
+                    return res.status(400).json({ message: "Usuario exisstente con el mismo correo electrónico", code: 1 });
                 }
                 else {
-                    // Encriptar la contraseña antes de agregar el usuario
                     const encryptedText = yield utils_1.utils.hashPassword(usuario.password);
                     usuario.password = encryptedText;
-                    // Agregar usuario si no existe
                     const result = yield usuarioModelo_1.default.add(usuario);
                     return res.json({ message: "Usuario agregado correctamente", code: 0 });
                 }
             }
             catch (error) {
-                return res.status(500).json({ message: `${error.message}` });
+                return res.status(500).json({ message: '${error.message}' });
             }
         });
     }
